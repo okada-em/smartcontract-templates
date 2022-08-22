ed9d3a93c6c577a8669736e5c22d42b051fb2cef
okada-em <k.okada@extramile.jp>
2022-08-22 15:22:26 +0900

    NBGUToken コントラクト作成

--- contracts/NBGUToken.sol
+++ contracts/NBGUToken.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
@@ -7,8 +7,7 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 contract ERC20Token is ERC20 {
     using SafeERC20 for IERC20;
 
-    constructor() ERC20("okada Token", "OKD"){
-        _mint(msg.sender, 100000 * 10 ** decimals());
-    }
+    constructor() ERC20("Neobred Game Utility Token", "NBGU"){}
 }
+
 
